d5a88088ae898d36fd02ceef950b64575d5d6445
sami5671 <samialam5671@gmail.com>
2023-09-15 00:18:34 +0600

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/Home/Home";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="header">Course Registration</h1>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
