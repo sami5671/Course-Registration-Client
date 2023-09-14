940d4dcdfd6d02930cb7cad54a9fea2f935ebda0
sami5671 <samialam5671@gmail.com>
2023-09-15 01:48:59 +0600

    1. Data fetched from public JSON file

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Cart.css";
+const Cart = () => {
+  return (
+    <div className="cart-container">
+      <h4>Credit Hour Remaining 0 hr</h4>
+      <hr />
+      <h3>Course Name</h3>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,41 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
+import "./Home.css";
 const Home = () => {
+  // ==============================React State declaration===================================
+  const [allCourses, setAllCourses] = useState([]);
+  // =========================Fetching the data from Public(data.json) file step 1========================================
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setAllCourses(data));
+  }, []);
+  //   console.log(allCourses);
   return (
-    <div>
-      <h1>Hello world!!!!</h1>
+    <div className="container">
+      <div className="home-container">
+        <div className="card-container">
+          {allCourses.map((course) => (
+            <div key={course.id} className="card">
+              <div className="card-img">
+                <img className="photo" src={course.image} alt="" />
+              </div>
+              <h3>{course.heading}</h3>
+              <p>
+                <small>{course.des}</small>
+              </p>
+              <div className="information">
+                <p>Price: {course.price} </p>
+                <p>Credit Hour: {course.credit_hour} hr</p>
+              </div>
+              <button className="card-btn">Select</button>
+            </div>
+          ))}
+        </div>
+        <div className="cart">
+          <Cart></Cart>
+        </div>
+      </div>
     </div>
   );
 };
